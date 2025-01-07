3713d0f57c9edf8f0bbde5449fcbac9c8d0b05ed
Rama Diaz <ramadiaz221@gmail.com>
2025-01-07 12:29:11 +0700

    UPDATE of VisitorChart

--- src/components/VisitorChart/index.tsx
+++ src/components/VisitorChart/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
+import { TrendingDown, TrendingUp } from "lucide-react";
 import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 
 import {
@@ -34,13 +34,20 @@ interface ChartDataType {
   reservations: number;
 }
 
+interface YearlyAnalytic {
+  period: string;
+  trend_percentage: number;
+  monthly_reservation: ChartDataType[];
+}
+
 export function VisitorChart() {
-  const [chartData, setChartData] = useState<ChartDataType[]>([]);
+  const [analyticData, setAnalyticData] = useState<YearlyAnalytic>();
+  const [isTrendingUp, setIsTrendingUp] = useState(true);
 
   const fetchData = async () => {
     try {
       const res = await fetchWithAuth(
-        BASE_API + "/partner/analytic/monthly-reservation",
+        BASE_API + "/partner/analytic/yearly-reservation",
         {
           method: "GET",
         }
@@ -48,7 +55,8 @@ export function VisitorChart() {
 
       const data = await res.json();
       if (res.ok) {
-        setChartData(data.data);
+        setAnalyticData(data.data);
+        setIsTrendingUp(data.data.trend_percentage >= 0);
       } else {
         toast.error(data.message);
       }
@@ -66,15 +74,13 @@ export function VisitorChart() {
     <Card className="w-full h-full bg-slate-50 rounded-lg p-2 shadow-lg shadow-sky-800/30">
       <CardHeader>
         <CardTitle>Reservation Analytics</CardTitle>
-        <CardDescription>
-          {chartData[0]?.month} - {chartData[chartData.length - 1]?.month}
-        </CardDescription>
+        <CardDescription>{analyticData?.period}</CardDescription>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
           <LineChart
             accessibilityLayer
-            data={chartData}
+            data={analyticData?.monthly_reservation}
             margin={{
               left: 12,
               right: 12,
@@ -103,8 +109,18 @@ export function VisitorChart() {
         </ChartContainer>
       </CardContent>
       <CardFooter className="flex-col items-start gap-2 text-sm">
-        <div className="flex gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        <div
+          className={`flex gap-2 font-medium leading-none ${
+            isTrendingUp ? "text-emerald-500" : "text-red-500"
+          }`}
+        >
+          Trending {isTrendingUp ? "Up" : "Down"} by{" "}
+          {analyticData?.trend_percentage}% this month{" "}
+          {isTrendingUp ? (
+            <TrendingUp className="h-4 w-4" />
+          ) : (
+            <TrendingDown className="h-4 w-4" />
+          )}
         </div>
         <div className="leading-none text-muted-foreground">
           Showing total reservations for the last 12 months
