537600db42416a979fc0fe91dcc9803ccd411df6
Rama Diaz <ramadiaz221@gmail.com>
2025-01-07 14:11:17 +0700

    MINOR UPDATE

--- src/app/(partner-area)/dashboard/page.tsx
+++ src/app/(partner-area)/dashboard/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { BaseContainer } from "@/components/BaseContainer";
 import { OrderAnalytic } from "@/components/OrderAnalytic";
 import { HotelTable } from "@/components/HotelTable";
 import { VisitorChart } from "@/components/VisitorChart";
@@ -9,7 +8,7 @@ const Page = () => {
   return (
     <div className="">
       <div className="flex flex-row gap-4">
-        <div className="w-1/2">
+        <div className="w-1/2 h-max">
           <VisitorChart />
         </div>
         <div className="w-1/2 h-max space-y-4">
