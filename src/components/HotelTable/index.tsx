0b69bdfb05b6cd53afb58fbfc1b55ce9c1dff4fc
Rama Diaz <ramadiaz221@gmail.com>
2025-01-07 14:41:04 +0700

    UPDATE of HotelTable component

--- src/components/HotelTable/index.tsx
+++ src/components/HotelTable/index.tsx
@@ -1,95 +1,116 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import fetchWithAuth from "@/utilities/fetchWithAuth";
+import { BASE_API } from "@/utilities/environment";
+import { IoIosStar } from "react-icons/io";
+import Link from "next/link";
+import { GoArrowUpRight } from "react-icons/go";
 
 export function HotelTable() {
+  const [hotelData, setHotelData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetchWithAuth(BASE_API + "/partner/hotel/getall", {
+        method: "GET",
+      });
+
+      const data = await res.json();
+      if (res.ok) {
+        setHotelData(data.data);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (err) {
+      console.error(err);
+      toast.error("Connection failed");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Card className="w-full h-full bg-slate-50 rounded-lg p-2 shadow-lg shadow-sky-800/30">
       <CardHeader>
         <CardTitle>Your Hotels</CardTitle>
         <CardDescription>A list of your registered hotels.</CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="overflow-x-scroll">
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[100px]">Invoice</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
+              <TableHead className="min-w-[200px]">Name</TableHead>
+              <TableHead className="text-right">Rating</TableHead>
+              <TableHead className="text-right">Cleanliness</TableHead>
+              <TableHead className="text-right">Comfort</TableHead>
+              <TableHead className="text-right">Service</TableHead>
+              <TableHead className="text-right">Facilities</TableHead>
+              <TableHead className="text-center">Act</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {invoices.map((invoice) => (
-              <TableRow key={invoice.invoice}>
-                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                <TableCell>{invoice.paymentStatus}</TableCell>
-                <TableCell>{invoice.paymentMethod}</TableCell>
-                <TableCell className="text-right">
-                  {invoice.totalAmount}
+            {hotelData.map((item: any, index) => (
+              <TableRow key={index}>
+                <TableCell className="font-medium">{item.name}</TableCell>
+                <TableCell className="">
+                  <div className="flex flex-row gap-1 items-center justify-end">
+                    {item.review_statistic.average_rating}
+                    <IoIosStar color="#f59e0b" size={14} />
+                  </div>
+                </TableCell>
+                <TableCell className="">
+                  <div className="flex flex-row gap-1 items-center justify-end">
+                    {item.rating.cleanliness}
+                    <IoIosStar color="#f59e0b" size={14} />
+                  </div>
+                </TableCell>
+                <TableCell className="">
+                  <div className="flex flex-row gap-1 items-center justify-end">
+                    {item.rating.comfort}
+                    <IoIosStar color="#f59e0b" size={14} />
+                  </div>
+                </TableCell>
+                <TableCell className="">
+                  <div className="flex flex-row gap-1 items-center justify-end">
+                    {item.rating.service_quality}
+                    <IoIosStar color="#f59e0b" size={14} />
+                  </div>
+                </TableCell>
+                <TableCell className="">
+                  <div className="flex flex-row gap-1 items-center justify-end">
+                    {item.rating.facilities}
+                    <IoIosStar color="#f59e0b" size={14} />
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <Link
+                    href={`https://nganterin.web.id/detail/hotel/${item.id}`}
+                    target="_blank"
+                    className="flex items-center justify-center"
+                  >
+                    <GoArrowUpRight size={14} />
+                  </Link>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell colSpan={3}>Total</TableCell>
-              <TableCell className="text-right">$2,500.00</TableCell>
-            </TableRow>
-          </TableFooter>
         </Table>
       </CardContent>
     </Card>
