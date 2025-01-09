b64c0d4c287ff7beceb3f0b1e58c75a824e32de6
Rama Diaz <ramadiaz221@gmail.com>
2025-01-09 18:33:09 +0700

    INIT of /auth/verify

---
+++ src/app/(public)/auth/verify/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { BASE_API } from "@/utilities/environment";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+const Page = () => {
+  const [status, setStatus] = useState<string>("loading");
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const token = useSearchParams().get("token");
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        BASE_API + `/partner/auth/verify?token=${token}`,
+        {
+          method: "POST",
+          cache: "no-store",
+        }
+      );
+
+      if (res.ok) {
+        setStatus("success");
+      } else {
+        const data = await res.json();
+        setErrorMessage(data.message);
+        setStatus("error");
+      }
+    } catch (err) {
+      console.error(err);
+      toast.error("Connection error");
+    }
+  };
+
+  const statusMessage = {
+    loading: "Verifying your verification token...",
+    success: "Partner email verified! Please login to start explore!",
+    error: "Something went wrong.",
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [token]);
+
+  return (
+    <div className="w-max mx-auto pt-[25vh]">
+      <div>
+        <h2 className="uppercase text-sky-600 text-5xl font-bold">
+          {status !== "error" ? status : errorMessage}
+        </h2>
+        <p>{statusMessage[status as keyof typeof statusMessage]}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
